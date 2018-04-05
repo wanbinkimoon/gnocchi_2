0b0907b3379cb8ab0a6d0b5e41122b4d9e7dc520
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2018-04-06 01:23:40 +0200

    minor stuff

--- src/Scenes/scene2.js
+++ src/Scenes/scene2.js
@@ -4,17 +4,16 @@ export default (s, p) => {
   const space = 4
   const move = p.frameCount * space
   const side = s.width
-  s.stroke(move * 2, 100, 100);
+  s.stroke(move, 100, 100);
 
-  s.line(0, move, move, move)
-  s.line(move, 0, move, move)
+  // s.line(0, move, move, move)
+  // s.line(move, 0, move, move)
 
   s.line(side, side - move, side - move, side - move)
   s.line(side - move, side, side - move, side - move)
-  s.line(side, 0, side - move, side - move)
-  s.line(side, side, side - move, side - move)
 
-  console.log(side - move, side, side - move, side - move)
+  s.line(side, move, side - move, side - move)
+  s.line(move, side, side - move, side - move)
   
   // s.line(move, 0, move, move)
   // s.line(move, move, 0, side)
